0e104874dab1903d2180092529a82c9574bab477
OneLoneFox <lonefoxkk@gmail.com>
2025-03-03 12:38:22 -0600

    Explicitly declare Plugin as the return type of createKonponents

--- packages/konponents-next/src/createKonponents.ts
+++ packages/konponents-next/src/createKonponents.ts
@@ -1,11 +1,11 @@
-import type { App } from "vue";
+import type { App, Plugin } from "vue";
 
 export interface CreateKonfig {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	components?: Record<string, any>
 }
 
-export function createKonponents(config: CreateKonfig) {
+export function createKonponents(config: CreateKonfig): Plugin {
 	return {
 		install(app: App) {
 			const { components } = config;
