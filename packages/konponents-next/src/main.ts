c076952bee13105dc8724418206ab4702d5a70c1
OneLoneFox <lonefoxkk@gmail.com>
2025-03-03 17:23:19 -0600

    Slight formatting change

--- packages/konponents-next/src/main.ts
+++ packages/konponents-next/src/main.ts
@@ -2,11 +2,9 @@ import * as components from "./components";
 import { type CreateKonfig, createKonponents as _create } from "./createKonponents";
 import "./styles/style.css";
 
-export function createKonponents(config: CreateKonfig){
+export function createKonponents(config: CreateKonfig) {
 	const userConfig: CreateKonfig = { components: components, ...config };
 	return _create(userConfig);
 }
 
-export {
-	components,
-};
+export { components };
